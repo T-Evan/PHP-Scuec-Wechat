d8d3853d2c9fd7277a2ecc209b0c886e10579b7f
yiwan <loopist@outlook.com>
2018-06-16 01:27:02 +0800

    fix route

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::any('/wechat', 'WeChatController@serve');
+Route::any('wechat', 'WeChatController@serve');
