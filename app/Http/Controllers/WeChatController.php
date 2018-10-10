f15d6962b0cce2101e41a79c28d2bece0cd51a54
yaphper <itsl@foxmail.com>
2018-10-11 01:15:05 +0800

    fix: A bug maybe; DEPLOY_NOW

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -26,7 +26,8 @@ class WeChatController extends Controller
     public function serve()
     {
         $app =  app('wechat');
-        $app->server->push(OtherMessageHandler::class); //优先过滤不能处理的消息类型，放在第一行防止错误覆盖其他正常信息处理结果
+        // a hidden bug ?
+        // $app->server->push(OtherMessageHandler::class); //优先过滤不能处理的消息类型，放在第一行防止错误覆盖其他正常信息处理结果
         $app->server->push(EventInfoHandler::class); //处理微信事件
         $app->server->push(TextMessageHandler::class, Message::TEXT); // 处理文字消息
         $app->server->push(ImageMessageHandler::class, Message::IMAGE); // 处理图片消息
