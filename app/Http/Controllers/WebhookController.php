475479b13bfc0dd3dc160016e047230fc8058366
yaphper <itsl@foxmail.com>
2018-10-13 13:10:05 +0800

    feature: add webhook controller and route; DEPLOY_NOW

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class WebhookController extends Controller
+{
+    private $BASE_PATH = '';
+
+    const REPOSITORY_FULL_NAME  = 'zixunminda/iscuecer';
+    const REPOSITORY_UUID       = '{b6f9ee1a-f707-4be6-89b4-5c4da9216d4a}';
+    const DEPLOY_KEYWORD        = 'DEPLOY_NOW';
+
+    public function __construct()
+    {
+        $this->BASE_PATH = base_path();
+    }
+
+    public function handler(Request $request)
+    {
+        if (checkHeader('X-Event-Key', 'repo:push')
+            && checkIP($request->getClientIp()) ) {
+            $jsonData = $request->input();
+            //check repository
+            if (
+                isset($jsonData['repository']['full_name'])
+                && isset($jsonData['repository']['uuid'])
+                && self::REPOSITORY_FULL_NAME === $jsonData['repository']['full_name']
+                && self::REPOSITORY_UUID === $jsonData['repository']['uuid']
+            ) {
+                // check the push message. The Message that contains "DEPLOY_NOW" will trigger a 'git pull' command
+                $changes = isset($jsonData['push']['changes']) ? $jsonData['push']['changes'] : false;
+                if (false !== $changes) {
+                    foreach ($changes as $each) {
+                        if (isset($each['new']) && isset($each['new']['target'])) {
+                            if (false !== strpos($each['new']['target']['message'], DEPLOY_KEYWORD)) {
+                                Log::debug("----webhook triggered-----");
+                                Log::debug(
+                                    "[git log]"
+                                    .shell_exec("cd {$this->BASE_PATH}; git pull 2>&1")
+                                );
+                                Log::debug(shell_exec("cd {$this->BASE_PATH}; composer install 2>&1"));
+                                Log::debug("----webhook closed-----");
+                                return 'success';
+                            }
+                        }
+                    }
+                } else {
+                    return 'no changes; do nothing;';
+                }
+            } else {
+                return response('Forbidden')->setStatusCode(403);
+            }
+        } else {
+            return response('Forbidden')->setStatusCode(403);
+        }
+    }
+
+    protected function checkHeader(string $header, string $assumption)
+    {
+        $header = str_replace('-', '_', strtoupper($header));
+        $eventKey = isset($_SERVER['HTTP_'.$header]) ? $_SERVER['HTTP_'.$header] : null;
+        if ($eventKey && $eventKey === $assumption) {
+            return true;
+        }
+        return false;
+    }
+
+    protected function checkIP(string $ip)
+    {
+        $fields = explode('.', $ip);
+        $validIPPrefix1 = ['104', '192', '136', '0'];
+        $validIPPrefix2 = ['34', '198', '203', '127'];
+        $validIPPrefix3 = ['34', '198', '178', '64'];
+        $validIPPrefix4 = ['34', '198', '32', '85'];
+        for ($i = 0; $i <= 1; ++$i) {
+            //bitbucket文档中给的ip段貌似有误不能完全匹配
+            if ($fields[$i] !== $validIPPrefix1[$i]
+                && $fields[$i] !== $validIPPrefix2[$i]
+                && $fields[$i] !== $validIPPrefix3[$i]
+                && $fields[$i] !== $validIPPrefix4[$i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,21 @@
 |
 */
 
+// webhook
+Route::get('webhook', 'WebhookController@handler');
+
 Route::get('/test1', 'StudentsController@test');
 
 Route::get('/students/create/{type}/{openid}', 'StudentsController@create')->name('students.create');
 Route::post('/students', 'StudentsController@store')->name('students.store');
 
-
 Route::any('/wechat', 'WeChatController@serve');
 
+// 用于修复微信文章 subscene 参数的 bug
 Route::get('/wechatArticleRedirect', 'WeChatController@redirectWechatArticle');
+
+// 大物实验
+Route::group(['prefix' => '/phy_exp'], function () {
+    // 绑定账号
+    Route::get('/bind_account', 'PhysicalExperimentController@bindAccountView');
+});
